6e614bfd66e52a833fcaeb648fee6d8bfba201ea
Nick Peters <npeters@omniupdate.com>
2013-08-10 16:20:39 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jat::Application.config.secret_token = '32f74ce03cb2b404414b829ef49df9a40905bc3c2f2ab932b7d41e3bb63eeea628d46daebaa6ec2b7c146927f88a8263a4da5e863ad7667ab1e43644cfcf1687'
